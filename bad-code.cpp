8728a897bf56abbd38b045f435b1b9c95b155134
Ghilezan19 <madalin.ghilezan@student.upt.ro>
2025-11-01 15:25:42 +0200

    Good code test

---
+++ bad-code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // CRITICAL ERROR: Hardcoded password
+    const char* password = "admin123";
+    
+    cout << "Password: " << password << endl;
+    
+    return 0;
+}
+
---
+++ good-code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    
+    if (x == 10) {  // Correct: == for comparison
+        cout << "Equal to 10" << endl;
+    }
+    
+    return 0;
+}
+
